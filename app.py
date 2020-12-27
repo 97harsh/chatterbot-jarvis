d0dbc4771c04253a3c4919a166735649f1ea7a58
97harsh <harsh25011997@hotmail.com>
2020-12-27 17:59:52 +0530

    testing basic app

--- app.py
+++ app.py
@@ -15,21 +15,21 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templates")
 app.config.from_object(os.environ['APP_SETTINGS'])
-chatbot = ChatBot('JARVIS',
-                  storage_adapter='chatterbot.storage.SQLStorageAdapter',
-                  logic_adapters=[
-                                  'chatterbot.logic.MathematicalEvaluation',
-                                  'chatterbot.logic.TimeLogicAdapter',
-                                  'chatterbot.logic.BestMatch',
-                                  {'import_path': 'chatterbot.logic.BestMatch',
-                                   'default_response': 'I am sorry, but I do not understand.\
-                                        I am still learning.',
-                                   'maximum_similarity_threshold': 0.90
-                                   }
-                  ])  # create chatbot
+# chatbot = ChatBot('JARVIS',
+#                   storage_adapter='chatterbot.storage.SQLStorageAdapter',
+#                   logic_adapters=[
+#                                   'chatterbot.logic.MathematicalEvaluation',
+#                                   'chatterbot.logic.TimeLogicAdapter',
+#                                   'chatterbot.logic.BestMatch',
+#                                   {'import_path': 'chatterbot.logic.BestMatch',
+#                                    'default_response': 'I am sorry, but I do not understand.\
+#                                         I am still learning.',
+#                                    'maximum_similarity_threshold': 0.90
+#                                    }
+#                   ])  # create chatbot
 
-trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train('chatterbot.corpus.english')
+# trainer = ChatterBotCorpusTrainer(chatbot)
+# trainer.train('chatterbot.corpus.english')
 
 
 @app.route("/")
@@ -40,15 +40,16 @@ def index():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get("msg")
-    return str(chatbot.get_response(userText))
+    # return str(chatbot.get_response(userText))
+    return f"Hi,  I am fine!!{UserText}"
 
 
-def main():
-    print(chatbot.get_response(""))
-    while True:
-        question = input()
-        response = chatbot.get_response(question)
-        print(response)
+# def main():
+#     print(chatbot.get_response(""))
+#     while True:
+#         question = input()
+#         response = chatbot.get_response(question)
+#         print(response)
 
 
 if __name__ == '__main__':
