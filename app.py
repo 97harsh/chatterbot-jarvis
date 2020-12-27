8c02870b2a7f8bc5be7f9ab85dbca17ef91a5d70
97harsh <harsh25011997@hotmail.com>
2020-12-27 18:41:10 +0530

    removed storage_adapter name,  sort sqlite

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__, template_folder="templates")
 app.config.from_object(os.environ['APP_SETTINGS'])
 print(os.environ['APP_SETTINGS'])
 chatbot = ChatBot('JARVIS',
-                  storage_adapter='chatterbot.storage.SQLStorageAdapter',
+                  #   storage_adapter='chatterbot.storage.SQLStorageAdapter',
                   logic_adapters=[
                                   'chatterbot.logic.MathematicalEvaluation',
                                   'chatterbot.logic.TimeLogicAdapter',
